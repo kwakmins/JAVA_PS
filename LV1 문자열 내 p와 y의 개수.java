edd94ece2a570f707b6af5c551ca75ddef9361c9
kawksung <alstjd5479@naver.com>
2023-02-02 17:48:20 +0900

    Create LV1 문자열 내 p와 y의 개수.java

---
+++ LV1 문자열 내 p와 y의 개수.java
@@ -0,0 +1,8 @@
+class Solution {
+
+  boolean solution(String s) {
+    String str = s.toLowerCase();
+    return str.chars().filter(c -> c == 'p').count() == str.chars().filter(c -> c == 'y').count();
+  }
+}
+
