6903163593c1ac649c369485895d14ff55e38aba
kawksung <alstjd5479@naver.com>
2023-06-11 23:52:47 +0900

    LV2 전화번호 목록

---
+++ LV2 전화번호 목록.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+
+  public boolean solution(String[] phone_book) {
+
+    Set<String> set = new HashSet<String>();
+
+    for (String phone : phone_book) {
+      set.add(phone);
+    }
+
+    for (String phone : phone_book) {
+      for (int i = 1; i < phone.length(); i++) {
+        if (set.contains(phone.substring(0, i))) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
