420303b6e6cecc8957f0aff54460e982195e12a5
kawksung <alstjd5479@naver.com>
2023-06-27 23:08:02 +0900

    LV1 가운데 글자 가져오기

---
+++ LV1 가운데 글자 가져오기.java
@@ -0,0 +1,17 @@
+class Solution {
+  public String solution(String s) {
+    String answer = "";
+
+    int len = s.length();
+    if(len % 2 == 0){
+      answer = Character.toString(s.charAt(len/2-1));
+      answer += Character.toString(s.charAt(len/2));
+
+    }
+    else{
+      answer = Character.toString(s.charAt(len/2));
+    }
+
+    return answer;
+  }
+}
