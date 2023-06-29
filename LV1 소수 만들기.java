20613725f7ba4ef81a11392d196c8f7bef288363
kawksung <alstjd5479@naver.com>
2023-06-29 23:48:28 +0900

    LV1 소수 만들기

---
+++ LV1 소수 만들기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+
+  public Boolean isPrime(int num) {
+    int cnt = 0;
+    for (int i = 1; i <= (int) Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        cnt += 1;
+      }
+    }
+    return cnt == 1;
+  }
+
+  public int solution(int[] nums) {
+    int answer = 0;
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      for (int j = i + 1; j < nums.length - 1; j++) {
+        for (int k = j + 1; k < nums.length; k++) {
+          if (isPrime(nums[i] + nums[j] + nums[k])) {
+            answer += 1;
+          }
+        }
+      }
+    }
+    return answer;
+  }
+
+}
