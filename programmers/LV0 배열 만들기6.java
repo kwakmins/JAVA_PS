29cebfed86122af69fe7abd6a387f402e5fc9300
kawksung <alstjd5479@naver.com>
2023-08-02 12:53:00 +0900

    LV0 배열 만들기6

---
+++ programmers/LV0 배열 만들기6.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+
+  public int[] solution(int[] arr) {
+    Stack<Integer> stk = new Stack<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (stk.empty()) {
+        stk.push(arr[i]);
+      } else {
+        if (stk.peek() == arr[i]) {
+          stk.pop();
+        } else {
+          stk.push(arr[i]);
+        }
+      }
+    }
+    if (stk.size() == 0) {
+      return new int[]{-1};
+    }
+    int[] answer = new int[stk.size()];
+    for (int i = stk.size() - 1; i >= 0; i--) {
+      answer[i] = stk.pop();
+    }
+    return answer;
+  }
+}
