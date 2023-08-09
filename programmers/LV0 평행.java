c46076173e8833b6b89f95ae950c4c368400e024
kawksung <alstjd5479@naver.com>
2023-08-09 10:17:20 +0900

    LV0 평행

---
+++ programmers/LV0 평행.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+
+  public int solution(int[][] dots) {
+    if ((double) Math.abs(dots[0][1] - dots[1][1]) / Math.abs(dots[0][0] - dots[1][0])
+        == (double) Math.abs(dots[2][1] - dots[3][1]) / Math.abs(dots[2][0] - dots[3][0])) {
+      return 1;
+    }
+    if ((double) Math.abs(dots[0][1] - dots[2][1]) / Math.abs(dots[0][0] - dots[2][0])
+        == (double) Math.abs(dots[1][1] - dots[3][1]) / Math.abs(dots[1][0] - dots[3][0])) {
+      return 1;
+    }
+    if ((double) Math.abs(dots[0][1] - dots[3][1]) / Math.abs(dots[0][0] - dots[3][0])
+        == (double) Math.abs(dots[1][1] - dots[2][1]) / Math.abs(dots[1][0] - dots[2][0])) {
+      return 1;
+    }
+    return 0;
+  }
+}
