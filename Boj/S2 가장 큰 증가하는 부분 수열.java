da47ed276574293104ea8ac284eb87577cfed596
kawksung <alstjd5479@naver.com>
2023-08-21 10:52:05 +0900

    S2 가장 큰 증가하는 부분 수열

--- Boj/S2 가장 큰 증가하는 부분 수열.java
+++ Boj/S2 가장 큰 증가하는 부분 수열.java
@@ -14,11 +14,12 @@ public class Main {
     int[] arr = Arrays.stream(bf.readLine().split(" ")).mapToInt(s -> Integer.parseInt(s))
         .toArray();
     int[] dp = new int[n];
+
     for (int i = 0; i < n; i++) {
-      dp[i] = 1;
+      dp[i] = arr[i];
       for (int j = 0; j < i; j++) {
-        if (arr[i] < arr[j]) {
-          dp[i] = Math.max(dp[i], dp[j] + 1);
+        if (arr[i] > arr[j]) {
+          dp[i] = Math.max(dp[i], dp[j] + arr[i]);
         }
       }
     }
