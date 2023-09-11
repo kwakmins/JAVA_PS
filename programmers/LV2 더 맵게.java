2c29f5c4955bd5b4269c2194efb7a0f47217aba5
kawksung <alstjd5479@naver.com>
2023-09-11 21:57:32 +0900

    LV2 더 맵게

---
+++ programmers/LV2 더 맵게.java
@@ -0,0 +1,53 @@
+/***
+ * O(N^2)은 값이 작지 않은 이상 시간 초과다.
+ * 가장 가까이? 이런게 Stack queue라는 힌트
+ * 왜 index를 stack에 넣을 생각을 못했찌?? 처음 해보는듯
+ */
+
+import java.util.Stack;
+
+class Solution {
+
+  public int[] solution(int[] numbers) {
+    Stack<Integer> stack = new Stack<>();
+    int[] answer = new int[numbers.length];
+    for (int i = 0; i < numbers.length; i++) {
+      while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+        answer[stack.pop()] = numbers[i];
+      }
+      stack.add(i);
+    }
+    for (int x : stack) {
+      answer[x] = -1;
+    }
+    return answer;
+  }
+
+  public int[] solution2(int[] numbers) {
+    int[] answer = new int[numbers.length];
+    int max = 0;
+    for (int i = 0; i < numbers.length; i++) {
+      max = max < numbers[i] ? numbers[i] : max;
+    }
+    for (int i = 0; i < numbers.length - 1; i++) {
+      if (numbers[i] == max) {
+        answer[i] = -1;
+        continue;
+      }
+      int temp = 0;
+      for (int j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] < numbers[j]) {
+          temp = numbers[j];
+          break;
+        }
+      }
+      if (temp != 0) {
+        answer[i] = temp;
+      } else {
+        answer[i] = -1;
+      }
+    }
+    answer[numbers.length - 1] = -1;
+    return answer;
+  }
+}
