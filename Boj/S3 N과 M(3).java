9ee6ac89b1dc1038f9c74cca5792564b2aaa0191
kawksung <alstjd5479@naver.com>
2023-10-04 15:46:12 +0900

    S3 N과 M(3)

---
+++ Boj/S3 N과 M(3).java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/***
+ *
+ */
+public class Main {
+
+  static int N, M;
+  static StringBuilder answer;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    String[] line = bf.readLine().split(" ");
+    answer = new StringBuilder();
+    N = Integer.parseInt(line[0]);
+    M = Integer.parseInt(line[1]);
+    dfs("", 0);
+    System.out.println(answer.toString());
+  }
+
+  static void dfs(String s, int dep) {
+    if (dep == M) {
+      answer.append(s).append("\n");
+      return;
+    }
+    for (int i = 1; i <= N; i++) {
+      dfs(s + i + " ", dep + 1);
+    }
+  }
+}
