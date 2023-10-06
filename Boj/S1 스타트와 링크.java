b94bd047a7c8eeec600d619e0a7a6b5ad1d4132f
kawksung <alstjd5479@naver.com>
2023-10-06 11:57:31 +0900

    S1 스타트와 링크
    .

---
+++ Boj/S1 스타트와 링크.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/***
+ *
+ */
+public class Main {
+
+  static int min = 123456, n;
+  static int[] visit;
+  static int[][] board;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    n = Integer.parseInt(bf.readLine());
+    visit = new int[n];
+    board = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+      for (int j = 0; j < n; j++) {
+        board[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    dfs(0, 0);
+    System.out.println(min);
+  }
+
+  static void dfs(int index, int dep) {
+    if (dep == n / 2) {
+      int start = 0;
+      int link = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+          if (visit[i] == 0 && visit[j] == 0) {
+            start += board[i][j] + board[j][i];
+          } else if (visit[i] == 1 && visit[j] == 1) {
+            link += board[i][j] + board[j][i];
+          }
+        }
+      }
+      min = Math.min(Math.abs(start - link), min);
+      if (min == 0) {
+        System.out.println(0);
+        System.exit(0);
+      }
+      return;
+    }
+
+    for (int i = index; i < n; i++) {
+      if (visit[i] == 0) {
+        visit[i] = 1;
+        dfs(i + 1, dep + 1);
+        visit[i] = 0;
+      }
+    }
+  }
+}
